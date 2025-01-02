1884914f5e56386fdd2ab6a2fe1a0dd9d47070c7
Eladio Castañeda <eladio.dev@gmail.com>
2025-01-02 13:04:42 -0400

    se generan ordenes a base de datos, empezamos a trabajar en eviar ordenes duplicadas

--- actions/create-order-actions.ts
+++ actions/create-order-actions.ts
@@ -1,21 +1,29 @@
-"use server"
+"use server";
 
+import { prisma } from "@/src/lib/prisma";
 import { OrderSchema } from "@/src/schema";
-;
 
-export async function createOrder (data: unknown) {
-    const result = OrderSchema.safeParse(data);
-    if (!result.success) {
-      return{
-          errors: result.error.issues
-      }
-    }
-    try {"generar Orden"}
-    catch (error) {
-      console.log(error);
-    }
+export async function createOrder(data: unknown) {
+  const result = OrderSchema.safeParse(data);
+  if (!result.success) {
+    return {
+      errors: result.error.issues,
+    };
   }
-    
-
-  
-
+  try {
+    await prisma.order.create({
+      data: {
+        name: result.data.name,
+        total: result.data.total,
+        orderItems: {
+          create: result.data.order.map((product: any) => ({
+            productId: product.id,
+            quantity: product.quantity,
+          })),
+        },
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
