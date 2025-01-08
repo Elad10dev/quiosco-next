32694a412468e9bab11766c85f30b1603e7abb83
Eladio Castañeda <eladio.dev@gmail.com>
2025-01-08 14:01:13 -0400

    se crea panel administrativo y los componentes para heading y logo

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,5 +1,6 @@
 import { prisma } from "@/src/lib/prisma"
 import  ProductCard from "@/components/products/productCard"
+import Heading from "@/components/ui/Heading"
 
 
 async function getProducts(category:string) {
@@ -21,9 +22,7 @@ export default async function OrderPage({params}: {params: {category: string}}) 
 
     return (
     <>
-      <h1
-      className="text-2xl my-10"
-      >Elige tu pedido a continuacion...🥰</h1>
+      <Heading>Elige tu pedido a continuacion...🥰</Heading>
 
 
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,5 +1,6 @@
 import { prisma } from "@/src/lib/prisma"
 import CatergoryIcon from "../ui/CatrgoryIcon"
+import Logo from "../ui/Logo"
 
 
 
@@ -13,6 +14,7 @@ export default async function OrderSidebar() {
   console.log(categories)
   return (
     <aside className="md:w-72 md:h-screen bg-white">
+      <Logo />
       <nav className="mt-10">
         {categories.map(category => (
           <CatergoryIcon
