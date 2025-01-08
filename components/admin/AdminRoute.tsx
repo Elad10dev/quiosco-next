a65487e96bda4cb67ef26f15aab63c19ae7b0b46
Eladio Castañeda <eladio.dev@gmail.com>
2025-01-08 16:47:42 -0400

    ultimos detalles para empezar a mostar pedidos en pantalla

--- components/admin/AdminRoute.tsx
+++ components/admin/AdminRoute.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+"use client"
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 type AdminRouteProps = {
   link:{
@@ -9,12 +10,13 @@ type AdminRouteProps = {
   }
 }
 export default function AdminRoute({link}: AdminRouteProps) {
+  const pathname = usePathname()
+  const isActive = pathname.startsWith(link.url)
   return (
     <Link
-    className={' font-bold text-lg border-t border-gray-200 p-3 last-of-type:border-b'}
+    className={`${isActive ? 'bg-amber-400' : ''} font-bold text-lg border-t border-gray-200 p-3 last-of-type:border-b`}
     href={link.url}>
       {link.text}
     </Link>
-    
   )
 }
