56a02df7b71473e73bee6d9d583270070dc0cf6e
Eladio Castañeda <eladio.dev@gmail.com>
2025-01-09 04:43:53 -0400

    se trabajo con cosultas de ordenes, se hacen las solicitues y se muestran en pantalla de cocina, comenzamos con elboton de pedido lito para enviar a otra pantalla

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,10 +1,51 @@
+import OrderCard from "@/components/order/OrderCard";
 import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
+import { OrderWithProducts } from "@/src/types";
+
+type OrderCardProps = { order: OrderWithProducts };
 
 
-export default function OrdersPage() {
+async function getPendingOrders() {
+  const orders = await prisma.order.findMany({
+    where: {
+      status: false
+    },
+    include: {
+      orderItems: {
+        include: {
+          product: true
+        }
+      }
+    }
+  });
+  return orders;
+}
+
+export default async function OrdersPage() {
+  const orders = await getPendingOrders();
+  
   return (
-    <div>
-      <Heading>Administrar Ordenes</Heading>
-    </div>
+    <>
+      <Heading> Administar Ordenes </Heading>
+      {orders.length ? (
+        <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5">
+          {orders.map(order => (
+          <OrderCard
+          key={order.id}
+          order={{
+          ...order,
+          orderProducts: order.orderItems.map(item => ({
+          ...item,
+          product: item.product
+      }))
+    }}
+  />
+))}
+        </div>
+      ) : (
+        <p>No hay ordenes pendientes</p>
+      )}
+    </>
   )
 }
