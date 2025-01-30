1db8654618ed01c6410837db84ced393134c9cc0
Eladio Castañeda <eladio.dev@gmail.com>
2025-01-30 11:57:01 -0400

    Listo para deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return (
-    <h1>Quiosco Next.js con App Router y Prisma, para Fast Food</h1>
-  );
+  redirect('/order/cafe')
 }
