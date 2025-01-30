2f348aab183416057d916894bdfa7cbc9f1a67fd
Eladio Castañeda <eladio.dev@gmail.com>
2025-01-30 07:23:18 -0400

    se agregan productos a DB comenzamos con panel para editar productos existentes

--- components/products/productCard.tsx
+++ components/products/productCard.tsx
@@ -1,7 +1,7 @@
 
 
 import { Product } from "@prisma/client";
-import { formatCurrency } from "../../src/util/index";
+import { formatCurrency, getImagenPath } from "../../src/util/index";
 import Image from "next/image";
 import AddProductButton from "./AddProductButton";
 
@@ -10,9 +10,10 @@ type ProductCardProps = {
   product:Product
 };
 export default function ProductCard({product}:ProductCardProps) {
+  const imagePath = getImagenPath(product.image);
   return (
     <div className="border bg-white">
-      <Image src={`/products/${product.image}.jpg`} alt={product.name} width={400} height={600}  />
+      <Image src={imagePath} alt={product.name} width={400} height={600}  />
       <div className="p-5">
         <h3 className="text-2xl font-bold"> {product.name} </h3>
         <p className="mt-5 font-black text-4xl text-amber-500">{formatCurrency(product.price)}</p>
--- src/util/index.ts
+++ src/util/index.ts
@@ -3,3 +3,12 @@
 export  function formatCurrency(amount: number) {
   return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(amount); 
 }
+
+export function getImagenPath(imagePath:string){
+  const cloudinaryBaseUrl = 'https://res.cloudinary.com/';
+  if(imagePath.includes(cloudinaryBaseUrl)){
+    return imagePath;
+  }else {
+    return `/products/${imagePath}.jpg`
+  }
+}
