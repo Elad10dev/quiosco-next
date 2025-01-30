c4981e077534bc14683c2e91ebc8ea387c273b73
Eladio Castañeda <eladio.dev@gmail.com>
2025-01-30 04:37:31 -0400

    se valida la creacion de productos y se muestrar errores con toast, se agrego schema de productos a zod

--- components/products/AddProductForm.tsx
+++ components/products/AddProductForm.tsx
@@ -1,13 +1,33 @@
-import ProductForm from "./ProductForm";
+"use client"
+
+import { ProductSchema } from "@/src/schema"
+import { toast } from "react-toastify"
 
 
-export default function AddProductForm() {
-  return (
+export default function AddProductForm({children}: {children: React.ReactNode}) {
+
+    const handleSubmit = async(FormData: FormData) => {
+        const data = {
+            name: FormData.get("name"),
+            description: FormData.get("description"),
+            price: FormData.get("price"),
+            image: FormData.get("image"),
+        }
+        const result = ProductSchema.safeParse(data)
+        console.log(result)
+        if (!result.success) {
+            result.error.issues.forEach((issue) => toast.error(issue.message));
+            return;
+        }
+    }
+
+    return (
     <div className='bg-white mt-10 px-5 py-10 rounded-md shadow-md max-w-3xl mx-auto'>
       <form
         className="space-y-5"
+        action={handleSubmit}
       >
-            <ProductForm />
+            {children}
 
             <input 
                 type="submit"
